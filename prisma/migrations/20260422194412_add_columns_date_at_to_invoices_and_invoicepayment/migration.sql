637bb8d04a944a9dbd82a7f9137636e27734da5f
Samuel Rosales Laptop <samuelrosales.dias@gmail.com>
2026-04-22 15:44:32 -0400

    feat(invoice): add date_at column to Invoice and InvoicePayment models

---
+++ prisma/migrations/20260422194412_add_columns_date_at_to_invoices_and_invoicepayment/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Invoice" ADD COLUMN     "date_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "InvoicePayment" ADD COLUMN     "date_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "Purchase" ALTER COLUMN "date" DROP NOT NULL;
