c48176b9be14e77103b5aa6ab8b8b0015efe1cd7
Samuel Rosales Laptop <samuelrosales.dias@gmail.com>
2026-05-23 15:18:21 -0400

    fix(app): import express correctly and ensure it's initialized

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { config } from 'dotenv';
+import express from 'express';
 config({ path: '.env' });
 
 import { Server } from './server/server.server';
 import { startExchangeRateCron } from "@/cron/exchangeRate.cron";
+
+void express;
 
 const server = new Server();
 
